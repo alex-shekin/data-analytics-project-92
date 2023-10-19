a49a67341fedde4edaa435b995118643def1bbb4
Alexander Shchekin <122755299+alex-shekin@users.noreply.github.com>
2023-10-19 19:16:25 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,2 @@
+select count(customer_id) as customers_count from customers; /* Подсчет количества ID покупателей 
+из таблицы customers при помоши функции count и присваивание имени столбцу при помощи as*\
